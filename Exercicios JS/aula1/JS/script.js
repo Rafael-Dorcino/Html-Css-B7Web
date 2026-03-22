c8bb6d69a8e1fbe4b3eb96a74a783312573c892a
RAFAEL DORCINO <163371790+Rafael-Dorcino@users.noreply.github.com>
2026-03-22 17:03:11 -0300

    começando no javascript

---
+++ Exercicios JS/aula1/JS/script.js
@@ -0,0 +1,17 @@
+console.log("Ola mundo 3.0")
+
+let nome = "Rafael"
+let idade = 23
+let estado = "Goias"
+let cidade = "Trindade"
+let logado = true
+let deslogado = false
+let lista = ["ovo", "leite", "margarina", "fermento", "acucar"]
+let nomecompleto = { nome: "Rafael", idade: 23}
+
+console.log(nome, idade)
+console.log(estado)
+console.log(cidade)
+console.log(lista)
+console.log(nomecompleto)
+console.log(typeof lista)
