8c7fc0e3b63cd1a717f9b33cfcade5dc93710718
RAFAEL DORCINO <163371790+Rafael-Dorcino@users.noreply.github.com>
2026-03-25 17:05:45 -0300

    Condicional If / else

---
+++ Exercicios JS/aula3/JS/script.js
@@ -0,0 +1,16 @@
+let idade = 18
+
+/*
+Na programação com JavaScript, estrutura condicional é um bloco de código que permite executar ações com base em uma condição.
+
+SE idade > 17
+console.log("Você é maior de idade")
+CASO CONTRÁRIO
+console.log("Você é menor de idade")
+*/
+
+if (idade >= 18){
+    console.log("Você é MAIOR de idade")
+}else {
+    console.log("Você é MENOR de idade")
+}
